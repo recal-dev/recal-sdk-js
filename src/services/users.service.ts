98cf1fd0182703ef8e63911adb1f89d93f5a8692
Torben Köhler <torben@peoplebrandingcompany.com>
2025-08-16 17:48:12 +0200

    Update users.service.ts

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -2,7 +2,7 @@ import { Type as T } from '@sinclair/typebox'
 import { includesHelper } from 'src/utils/includes.helper'
 import { User } from '../entities/user'
 import { userSchema } from '../typebox/user.tb'
-import { errorHandler, FetchError, type FetchHelper } from '../utils/fetch.helper'
+import { errorHandler, type FetchHelper } from '../utils/fetch.helper'
 
 interface UserOptions {
     includeOrgs: boolean
@@ -12,11 +12,21 @@ interface UserOptions {
 export class UsersService {
     constructor(private fetchHelper: FetchHelper) {}
 
+    /**
+     * List all users
+     * @returns An array of users
+     */
     public async listAll(): Promise<User[]> {
         const users = await this.fetchHelper.get('/v1/users', { schema: T.Array(userSchema) })
         return users.map((user) => User.fromJson(user))
     }
 
+    /**
+     * Get a user by ID
+     * @param id The ID of the user
+     * @param options Options for the user
+     * @returns The user
+     */
     public async get(id: string, { includeOrgs = false, includeOAuth = false }: UserOptions): Promise<User | null> {
         const newUser = await this.fetchHelper
             .get(`/v1/users/${id}`, {
@@ -32,6 +42,12 @@ export class UsersService {
         return newUser ? User.fromJson(newUser) : null
     }
 
+    /**
+     * Create a new user
+     * @param id The ID of the user
+     * @param organizationSlugs The slugs of the organizations the user is a member of
+     * @returns The new user
+     */
     public async create(id: string, organizationSlugs?: string[]): Promise<User> {
         const newUser = await this.fetchHelper.post(`/v1/users`, {
             body: { id, organizationSlugs },
