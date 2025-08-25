54a0ca9a97fc4af10e5665e8f233e6a4a2a5c154
Torben Köhler <torben@peoplebrandingcompany.com>
2025-08-25 10:22:15 +0200

    Delete typedi.ts

--- src/typedi.ts
+++
@@ -1,4 +0,0 @@
-import { Container } from 'typedi'
-
-export const getTimeZone = () => Container.get<string>('timezone')
-export const setTimeZone = (timezone: string) => Container.set('timezone', timezone)
