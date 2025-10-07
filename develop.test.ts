c16b1f53b431fcbe72330d20a765f6e8d258c548
Jona Schwarz <jonaschwarz1@gmail.com>
2025-10-07 22:37:37 +0200

    remove todos and method call

--- develop.test.ts
+++ develop.test.ts
@@ -1,13 +1,6 @@
 import { RecalClient } from './src/index'
 
 const recalClient = new RecalClient({
-    token: 'recal_1234567890',
+    token: process.env.RECAL_TOKEN,
+    url: process.env.RECAL_URL,
 })
-
-const connections = await recalClient.oauth.getAllConnections('1234567890')
-console.log(connections)
-// TODOs
-// - add missing bulk scheduling method
-// - add missing methods for users/orgs
-// - test all methods (manually first)
-// - test all methods (automated against staging api)
